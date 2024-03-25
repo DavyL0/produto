5824ec61260ec798b636e592d7f57852680df031
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2024-03-25 10:28:43 -0300

    Finalização do Projeto de API com Java Spring, ela funciona normalmente adicionando, modificando e eliminando cadastros do banco de dados. Ultima alterações: Criação do DELETE e também de uma class de  mensagem de sucesso e error

--- src/main/java/com/api/produto/controle/ProdutoControle.java
+++ src/main/java/com/api/produto/controle/ProdutoControle.java
@@ -3,6 +3,7 @@ package com.api.produto.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.api.produto.repositorio.*;
 import com.api.produto.modelo.ProdutoModelo;
+import com.api.produto.modelo.RepostaModelo;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +31,32 @@ public class ProdutoControle {
     @RequestMapping(value = "/produtos", method = RequestMethod.POST)
     public @ResponseBody ProdutoModelo cadastrar(@RequestBody ProdutoModelo produto){
         return acoes.save(produto);
+    }
+
+    //Filtrar produtos 
+    @RequestMapping(value = "/produtos/{codigo}", method = RequestMethod.GET)
+    public @ResponseBody ProdutoModelo filtrar(@PathVariable Integer codigo){
+        return acoes.findByCodigo(codigo);
+    }
+
+    //Alterar produtos
+    @RequestMapping(value = "/produtos", method = RequestMethod.PUT)
+    public @ResponseBody ProdutoModelo alterar(@RequestBody ProdutoModelo produto){
+        return acoes.save(produto);
+    }
+
+    //Remover produtos
+    @RequestMapping(value ="/produtos/{codigo}", method = RequestMethod.DELETE)
+    public @ResponseBody RepostaModelo remove(@PathVariable Integer codigo) {
+
+        RepostaModelo resposta = new RepostaModelo();
+        try{
+            ProdutoModelo produto = filtrar(codigo);
+            this.acoes.delete(produto);
+            resposta.setMensagem("Produto removido com sucesso");    
+        }catch(Exception erro){
+            resposta.setMensagem("Falha ao remover: " +erro.getMessage());
+        }
+        return resposta;
     }
 }
